a6639d679b45dfa512367995797aa01ef20780bd
saima <akramsaima459@gmail.com>
2024-08-17 10:38:03 +0530

    day20

--- Day19/Mapping.java
+++ Day19/Mapping.java
@@ -1,9 +1,5 @@
 import java.util.*;
 public class Mapping {
-
-   
-
-
      public static void main (String[] args){
         HashMap<Integer,Integer> mp=new HashMap<> (); //initialize
         int[] arr={1,2,3,4,5,2,1,2,3,3,6,7,6,18,3,2,2};
@@ -17,28 +13,28 @@ public class Mapping {
       }
       System.out.println(mp);
 
-      //   mp.put(1,2); //add
-      //   mp.put(2,3);
-      //   mp.put(3,4);
-      //   mp.put(4,1);
-      //   mp.put(5, null); //value of any key be defined with null
-      //   System.out.println(mp); //display
-      //   arr[0]=2;
-      //   arr[1]=4;
-      //   mp.put(1,1);
-      //   mp.put(4,2);
-      //   System.out.println(mp.containsKey(3)); //to check if a key already exists or not boolean
-      //   System.out.println(mp); //using put function to add or modify anything in map
-      //   mp.remove(3); //to remove 
-      //   System.out.println(mp);
-      //   System.out.println(mp.containsKey(3)); 
-      //   System.out.println(mp.get(2)); // accessing, gives null if the key doesnt exist and gives the value if key exists
+        mp.put(1,2); //add
+        mp.put(2,3);
+        mp.put(3,4);
+        mp.put(4,1);
+        mp.put(5, null); //value of any key be defined with null
+        System.out.println(mp); //display
+        arr[0]=2;
+        arr[1]=4;
+        mp.put(1,1);
+        mp.put(4,2);
+        System.out.println(mp.containsKey(3)); //to check if a key already exists or not boolean
+        System.out.println(mp); //using put function to add or modify anything in map
+        mp.remove(3); //to remove 
+        System.out.println(mp);
+        System.out.println(mp.containsKey(3)); 
+        System.out.println(mp.get(2)); // accessing, gives null if the key doesnt exist and gives the value if key exists
 
-      //   iteration of map
-      //   for(Map.Entry m:mp.entrySet()){  //getkey() getvalue()  m works like i and entrySet converts the map into a set     set is always sorted and no key is repeated
-      //       System.out.println("the key is :"+m.getKey() + " the value is:"+ m.getValue());
+       // iteration of map
+        for(Map.Entry m:mp.entrySet()){  //getkey() getvalue()  m works like i and entrySet converts the map into a set     set is always sorted and no key is repeated
+            System.out.println("the key is :"+m.getKey() + " the value is:"+ m.getValue());
 
-      //   }
+        }
         
      }
     
